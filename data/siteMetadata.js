00e71a7c72844a31bb33c2b943687bdc7a263a42
Francisco Javier Barrena <fjbarrena@gmail.com>
2023-02-19 20:56:18 +0100

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -13,9 +13,7 @@ const siteMetadata = {
   email: 'fjbarrena@gmail.com',
   github: 'https://github.com/fjbarrena',
   twitter: 'https://twitter.com/DogDeveloper',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com/in/fjbarrena',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
