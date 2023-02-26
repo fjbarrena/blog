b4da5154543bd59d240da474d8bc88769475706d
Fran Barrena <francisco@kyso.io>
2023-02-26 15:40:13 +0100

    feat: added two more projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -11,20 +11,19 @@ const projectsData = [
     description: `ZDMP offers an open Industry 4.0 environment where a new generation of developed zero-defect service applications will be available in a marketplace contributing to create an ecosystem where ZDMP stakeholders would be able to interact with each other.`,
     imgSrc: '/static/images/zdmp.png',
     href: 'https://www.zdmp.eu',
-  } /*
-  {
-    title: 'Nexica: The CloudManager to rule them all',
-    description: `Your organisation's knowledge is scattered across too many tools because no one solution allows everyone to share everything in the same place.
-    Until now.`,
-    imgSrc: '/static/images/kyso.png',
-    href: 'https://about.kyso.io',
   },
   {
-    title: 'Opossum: The API Security Platform powered by AI',
-    description: `ZDMP offers an open Industry 4.0 environment where a new generation of developed zero-defect service applications will be available in a marketplace contributing to create an ecosystem where ZDMP stakeholders would be able to interact with each other.`,
-    imgSrc: '/static/images/zdmp.png',
-    href: 'https://www.zdmp.eu',
-  },*/,
+    title: 'Nexica: The CloudManager to rule them all',
+    description: `Nexica Cloud Manager is the control panel for your cloud infrastructure, your mail services (mailboxes, antispam and Exchange) and domains, with multiple functions: creation, activation and restarting servers, setting the degree of elasticity, configuring firewall rules, etc.`,
+    imgSrc: '/static/images/nexica_cm.png',
+    href: 'https://nexica.com/en/cloud-expertise/cloud-manager',
+  },
+  {
+    title: 'Opossum: Cybersecurity based on Big Data and AI for business applications',
+    description: `The OPOSSUM project acts as a reverse proxy between users and applications. In this way, it is in a privileged position to apply security checks, much like a classic Web Application Firewall (WAF), but it is also in a privileged position to analyze how a user uses a given application, applying AI and User Behaviour to detect malicious patterns.`,
+    imgSrc: '/static/images/opossum.jpg',
+    href: 'https://www.iti.es/proyectosidi/opossum-securizacion-de-aplicaciones-basada-en-tecnicas-big-data-e-inteligencia-artificial/',
+  },
 ]
 
 export default projectsData
