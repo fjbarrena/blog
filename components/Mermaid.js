7dc188957a5be7ab6a275afd98a92901872bce70
Fran Barrena <francisco@kyso.io>
2023-06-26 16:43:54 +0200

    fix: mermaid id collision

--- components/Mermaid.js
+++ components/Mermaid.js
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import mermaid from 'mermaid'
+import { v4 as uuidv4 } from 'uuid'
 
 mermaid.initialize({
   startOnLoad: true,
   theme: 'dark',
   securityLevel: 'loose',
   deterministicIds: false,
-  deterministicIDSeed: new Date().getMilliseconds(),
 })
 
-export default class Mermaid extends React.Component {
-  componentDidMount() {
-    mermaid.contentLoaded()
-  }
-  render() {
-    return <div className="mermaid">{this.props.chart}</div>
-  }
+const Mermaid = (props) => {
+  const [svg, setSvg] = useState(null)
+
+  useEffect(async () => {
+    const theSvg = await mermaid.render('_' + uuidv4(), props.chart)
+    setSvg(theSvg.svg)
+  }, [])
+
+  return <div dangerouslySetInnerHTML={{ __html: svg }}></div>
 }
+
+export default Mermaid
