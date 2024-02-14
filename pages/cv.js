946ba40f3de6bd2eb81c7c1d0c83f0f2efad2072
Francisco Javier Barrena <fjbarrena@gmail.com>
2024-02-14 16:31:14 +0100

    feat: added link

--- pages/cv.js
+++ pages/cv.js
@@ -759,8 +759,8 @@ export default function About({ authorDetails }) {
                 <div className="sm:overflow-hidden sm:rounded-lg">
                   <div className="divide-y divide-gray-200">
                     <div className="px-3 py-5 sm:px-3">
-                      <a href="#speaker">
-                        <h2 name="speaker" id="notes-title" className="text-lg font-medium ">
+                      <a name="speaker" href="#speaker">
+                        <h2 id="notes-title" className="text-lg font-medium ">
                           Speaker at events
                         </h2>
                       </a>
